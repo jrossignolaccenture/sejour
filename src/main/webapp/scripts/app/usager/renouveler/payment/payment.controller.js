f9d49ec9eb3f2dc24c162c7ac59b371bc6e6f748
JulienRossignol <j.rossignol@accenture.com>
2015-10-12 15:41:10 +0200

    nettoyage images + autocomplete du payment

--- src/main/webapp/scripts/app/usager/renouveler/payment/payment.controller.js
+++ src/main/webapp/scripts/app/usager/renouveler/payment/payment.controller.js
@@ -3,7 +3,15 @@
 angular.module('sejourApp')
     .controller('RenouvelerPaymentController', function ($scope, $state, Principal, Demande) {
         
-        
+
+		$scope.autocomplete = function() {
+			if ($scope.payment.cardOwner === 'sief') {
+				$scope.payment.cardNumber = '4970 1012 3456 7890';
+				$scope.payment.cardExpiringDate = '08 / 16';
+				$scope.payment.cardCV = '123';
+			}
+		};
+    		
         $scope.save = function () {
         	console.log($scope.demande);
             Demande.prepaid().then(function(result){
