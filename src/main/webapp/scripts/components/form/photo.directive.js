1b330afd5e4730307143588b134f4a12d2093d35
JulienRossignol <j.rossignol@accenture.com>
2015-11-12 13:54:08 +0100

    ajout zoom et pan sur visionneuse photo

--- src/main/webapp/scripts/components/form/photo.directive.js
+++ src/main/webapp/scripts/components/form/photo.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('sejourApp')
-    .directive('photoViewer', function() {
+    .directive('photoViewer', function($timeout) {
         return {
             restrict: 'E',
             required: ['^ngModel'],
@@ -26,11 +26,30 @@ angular.module('sejourApp')
             	$scope.refresh();
             }],
             link: function(scope, element, attrs) {
+            	scope.zoomButtonClick = function(zoomOut) {
+             	   var scaleToAdd = 0.4;
+             	   if(zoomOut)
+             	      scaleToAdd = -scaleToAdd;
+             	   $("#img"+scope.documents[0].id).smartZoom("zoom", scaleToAdd);
+             	}
                 scope.$watch("model",function(newValue,oldValue) {
                 	if(oldValue != newValue) {
+                		$timeout(function() {
                 		scope.model = newValue;
                 		scope.refresh();
+                		});
                 	}
+                });
+                scope.$watch("currentIndex",function(newValue,oldValue) {
+                	if(oldValue != newValue) {
+                		$("#img"+scope.documents[0].id).smartZoom("zoom", -3);
+                	}
+                });
+                $timeout(function() {
+                	$("#img"+scope.documents[0].id).smartZoom({
+                		'scrollEnabled': false,
+                		'dblClickEnabled' : false
+                	});
                 });
              }
         };
