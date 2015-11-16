d165fcced2554e801f8e25d2bbcce2bacc260b94
JulienRossignol <j.rossignol@accenture.com>
2015-11-16 10:38:11 +0100

    modif connexion mongodb3 + correctif pour faire marcher le mode cloud

--- src/main/java/fr/minint/sief/config/CloudMongoDbConfiguration.java
+++ src/main/java/fr/minint/sief/config/CloudMongoDbConfiguration.java
@@ -1,22 +1,22 @@
 package fr.minint.sief.config;
 
-import com.mongodb.Mongo;
+import static java.util.Collections.singletonList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @Configuration
 @EnableMongoRepositories("fr.minint.sief.repository")
@@ -25,8 +25,23 @@ public class CloudMongoDbConfiguration extends AbstractMongoConfiguration  {
 
     private final Logger log = LoggerFactory.getLogger(CloudDatabaseConfiguration.class);
 
-    @Inject
-    private MongoDbFactory mongoDbFactory;
+//    @Inject
+//    private MongoDbFactory mongoDbFactory;
+    
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+    
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+    
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+    
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+    
+    @Value("${spring.data.mongodb.password}")
+    private String password;
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener() {
@@ -38,13 +53,28 @@ public class CloudMongoDbConfiguration extends AbstractMongoConfiguration  {
         return new LocalValidatorFactoryBean();
     }
 
+//    @Override
+//    protected String getDatabaseName() {
+//        try {
+//			return mongoDbFactory().getDb().getName();
+//		} catch (Exception e) {
+//			throw new RuntimeException(e);
+//		}
+//    }
+//
+//    @Override
+//    public Mongo mongo() throws Exception {
+//        return mongoDbFactory().getDb().getMongo();
+//    }
+    
     @Override
     protected String getDatabaseName() {
-        return mongoDbFactory.getDb().getName();
+        return database;
     }
 
     @Override
     public Mongo mongo() throws Exception {
-        return mongoDbFactory().getDb().getMongo();
+        return new MongoClient(singletonList(new ServerAddress(host, port)),
+                singletonList(MongoCredential.createCredential(username, database, password.toCharArray())));
     }
 }
--- src/main/java/fr/minint/sief/config/DatabaseConfiguration.java
+++ src/main/java/fr/minint/sief/config/DatabaseConfiguration.java
@@ -1,23 +1,26 @@
 package fr.minint.sief.config;
 
 
-import com.mongodb.Mongo;
-import org.mongeez.Mongeez;
+import static java.util.Collections.singletonList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import javax.inject.Inject;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @Configuration
 @Profile("!" + Constants.SPRING_PROFILE_CLOUD)
@@ -28,11 +31,26 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration  {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
 
-    @Inject
-    private Mongo mongo;
-
-    @Inject
-    private MongoProperties mongoProperties;
+//    @Inject
+//    private Mongo mongo;
+//
+//    @Inject
+//    private MongoProperties mongoProperties;
+    
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+    
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+    
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+    
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+    
+    @Value("${spring.data.mongodb.password}")
+    private String password;
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener() {
@@ -46,23 +64,24 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration  {
 
     @Override
     protected String getDatabaseName() {
-        return mongoProperties.getDatabase();
+        return database;
     }
 
     @Override
     public Mongo mongo() throws Exception {
-        return mongo;
+        return new MongoClient(singletonList(new ServerAddress(host, port)),
+                singletonList(MongoCredential.createCredential(username, database, password.toCharArray())));
     }
 
-    @Bean
-    @Profile("!" + Constants.SPRING_PROFILE_FAST)
-    public Mongeez mongeez() {
-        log.debug("Configuring Mongeez");
-        Mongeez mongeez = new Mongeez();
-        mongeez.setFile(new ClassPathResource("/config/mongeez/master.xml"));
-        mongeez.setMongo(mongo);
-        mongeez.setDbName(mongoProperties.getDatabase());
-        mongeez.process();
-        return mongeez;
-    }
+//    @Bean
+//    @Profile("!" + Constants.SPRING_PROFILE_FAST)
+//    public Mongeez mongeez() throws Exception {
+//        log.debug("Configuring Mongeez");
+//        Mongeez mongeez = new Mongeez();
+//        mongeez.setFile(new ClassPathResource("/config/mongeez/master.xml"));
+//        mongeez.setMongo(mongo());
+//        mongeez.setDbName(database);
+//        mongeez.process();
+//        return mongeez;
+//    }
 }
